3057b0295bfd125b697ab7a6e3ba76f634346861
LucasGiuberti <lucasgiuberti.a@gmail.com>
2022-02-15 19:15:41 -0300

    Adicionando mais matérias .

--- Learn_PHP/escopo/escopo_das_variaveis.php
+++ Learn_PHP/escopo/escopo_das_variaveis.php
@@ -1,1 +1,20 @@
 <?php
+$dinner = 'Curry Cuttlefish';
+
+function vegetarian_dinner(){
+    $dinner = 'Sauteed Pea Shoots';
+    print "Dinner is $dinner, or ";
+    print $dinner;
+    print "\n";
+}
+function kosher_dinner(){
+    $dinner = 'Kung Pao Chicken';
+    print "Dinner is $dinner. or";
+    print $dinner;
+        print "\n";
+}
+print "Vegetarian ";
+vegetarian_dinner();
+print "Kosher";
+kosher_dinner();
+print "Regular dinner is $dinner";
